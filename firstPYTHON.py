586f7ed75b17b855b334e4df4979883be19fe170
Monroe447456 <125819308+Monroe447456@users.noreply.github.com>
2023-02-19 12:15:37 -0500

    Create firstPYTHON.py
    This is my first time using Python and I am doing the 100 days coding challenge. This is one of my first-ever projects that originally was from Replit. I will keep uploading stuff from my Replit to prove that I have been indeed doing Python.

---
+++ firstPYTHON.py
@@ -0,0 +1,17 @@
+
+print ("hello")
+
+print ("Hello Replit")
+
+print ("This is my first time using a string!")
+
+print ("""hey how are you, I am good. Lets go have some fun and adventures. Additionally, lets go to the magical land of boo boo laa laa.""")
+
+
+
+
+print ("syntax error correct")
+
+print ("you need to print this sentence")
+
+print ("""Monroe. Febuary 17, 2023. I am signing up for Replit's 100 days of Python challenge! I will make sure to spend some time every day coding along, for a minimum of 12 minutes per day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field. I am feeling driven!""")
